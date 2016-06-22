0a4f96ee65a05a0ac8ab939a45499aff2de1286a
lxx <listen987654321@sina.com>
2016-06-22 14:48:04 +0800

    Initial Commit

---
+++ Runner/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Runner
+//
+//  Created by lxx on 16/6/22.
+//  Copyright © 2016年 lxx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Runner/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Runner
+//
+//  Created by lxx on 16/6/22.
+//  Copyright © 2016年 lxx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
